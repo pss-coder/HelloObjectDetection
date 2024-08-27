374247372a142a7123cc548a1a62b5a2bc0eb11d
Pawandeep Singh <pawandeepse@me.com>
2024-08-27 11:31:39 +0800

    Initial Commit

---
+++ HelloObjectDetection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloObjectDetection
+//
+//  Created by Pawandeep Singh Sekhon on 27/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloObjectDetection/HelloObjectDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloObjectDetectionApp.swift
+//  HelloObjectDetection
+//
+//  Created by Pawandeep Singh Sekhon on 27/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloObjectDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
